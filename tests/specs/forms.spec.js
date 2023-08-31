e4fd126e41acc9df558178f57b4db3687f29cd79
Uilton de Oliveira Rodrigues <uiltondeoliveira@gmail.com>
2023-08-31 18:32:46 -0300

    Adiciona resolução do exercício de teste mobile da EBAC

--- tests/specs/forms.spec.js
+++ tests/specs/forms.spec.js
@@ -4,18 +4,18 @@ describe('Acesso ao menu forms', () => {
     it('Deve relizar cadastro com sucesso', async () => {
 
         await formScreen.goToForm()
-        await driver.pause(500)
+        await driver.pause(1000)
         await formScreen.typeText('Meu texto aqui')
-        await driver.pause(500)
+        await driver.pause(1000)
         await formScreen.enableSwitch()
-        await driver.pause(500)
+        await driver.pause(1000)
         await formScreen.selectOptionIntoDropdown()
-        await driver.pause(500)
+        await driver.pause(1000)
         await formScreen.activeButton()
-        await driver.pause(500)
+        await driver.pause(1000)
 
         expect(await $('android.widget.FrameLayout')).toBeDisplayed()
-        await driver.pause(500)       
+        await driver.pause(1000)     
 
     });
 });
